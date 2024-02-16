0b7e70cec33808135b11a9bbeb7c612de55e3759
Rafiw Idrissu <dev.rafiu@gmail.com>
2024-02-16 10:21:51 +0000

    feat: style get-statrted section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
       {/* <Games /> */}
       {/* <Partners /> */}
       {/* <Subscription /> */}
-      <Reviews />
+      {/* <Reviews /> */}
       {/* <Faqs /> */}
-      {/* <GetStarted /> */}
+      <GetStarted />
     </div>
   );
 }
--- src/components/get-started.tsx
+++ src/components/get-started.tsx
@@ -2,35 +2,46 @@ import Image from "next/image";
 
 function GetStarted() {
   return (
-    <section>
-      <Image
-        src="/images/get-started/primary-img.png"
-        alt=""
-        width={400}
-        height={400}
-      />
-      <h2>Try EduPlay for free</h2>
-      Unlock knowledge through play. Dive into EduPlay&apos;s world of fun and
-      discovery. Ready for endless smiles and learning adventures?
-      <button>Get started for free</button>
-      <div>
+    <section className="container relative pt-12 space-y-8 pb-0">
+      <div className="flex lg:flex-row gap-4 lg:gap-12 flex-col-reverse justify-between">
+        <div className="w-full max-w-[48rem] h-[25rem] relative">
+          <Image
+            src="/images/get-started/primary-img.png"
+            alt=""
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="space-y-4 lg:pt-12">
+          <h2>Try EduPlay for free</h2>
+          <p className="max-w-lg">
+            Unlock knowledge through play. Dive into EduPlay&apos;s world of fun
+            and discovery. Ready for endless smiles and learning adventures?
+          </p>
+          <button className="bg-violet text-background py-2 px-3 rounded font-semibold text-sm w-full lg:w-max">
+            Get started for free
+          </button>
+        </div>
+      </div>
+
+      <div className="lg:flex items-center bottom-0 left-0 right-0 justify-between hidden px-8">
         <Image
           src="/images/get-started/img1.png"
           alt=""
-          width={40}
-          height={40}
+          width={70}
+          height={50}
         />
         <Image
           src="/images/get-started/img2.png"
           alt=""
-          width={40}
-          height={40}
+          width={70}
+          height={50}
         />
         <Image
           src="/images/get-started/img3.png"
           alt=""
-          width={40}
-          height={40}
+          width={70}
+          height={50}
         />
       </div>
     </section>
